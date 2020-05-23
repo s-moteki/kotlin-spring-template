33ee32debd1cf787cfbea7d0dfdba58ed3197446
s-moteki <f16bc008@gmail.com>
2020-05-23 22:39:20 +0900

    [add] コントローラーの追加

--- src/main/kotlin/com/sample/demo/application/admin/AdminController.kt
+++ src/main/kotlin/com/sample/demo/application/admin/AdminController.kt
@@ -1,10 +1,13 @@
 package com.sample.demo.application.admin
 
 import com.sample.demo.common.PageName
+import com.sample.demo.domain.service.UserService
 import org.springframework.stereotype.Controller
+import org.springframework.validation.BindingResult
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
 
 /**
  * 管理画面コントローラー
@@ -12,8 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping
  * @author moteki
  */
 @Controller
-@RequestMapping("/admin")
-class AdminController {
+class AdminController(
+  val userService: UserService
+) {
 
   /**
    * ログイン、TOP画面遷移
@@ -21,8 +25,10 @@ class AdminController {
    * @return 画面遷移
    */
   @GetMapping
-  fun index() = PageName.ADMIN_INDEX.key
-
+  fun index(loginAdminForm: LoginAdminForm, mav: ModelAndView): ModelAndView {
+    mav.viewName = PageName.ADMIN_INDEX.key
+    return mav
+  }
 
   /**
    * ログイン処理
@@ -30,6 +36,24 @@ class AdminController {
    * @return 画面遷移
    */
   @PostMapping("/login")
-  fun login() = PageName.ADMIN_PROFILE.key
+  fun login(@Validated loginAdminForm: LoginAdminForm, bindingResult: BindingResult, mav: ModelAndView): ModelAndView {
+    if (bindingResult.hasErrors()) return index(loginAdminForm, mav)
+
+    mav.viewName = PageName.ADMIN_PROFILE.key
+    val userDto = AdminFormHelper.to(loginAdminForm)
+
+    // TODO 存在チェックしかしていない
+    return if (userService.findUserByCondition(userDto).isEmpty()) {
+      index(loginAdminForm, mav)
+    } else {
+      mav
+    }
+  }
+
+  /**
+   * 引数のURLをリダイレクトURLにして返す
+   * @return リダイレクトURL
+   */
+  fun redirect(url: String) = "redirect:$url"
 
 }
